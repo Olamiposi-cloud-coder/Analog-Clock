7a02f207961b9a11defded353b9a0311fad85a02
Olamiposi Cloud <Olamiposi-cloud-coder>
2023-01-30 13:55:17 +0100

    analog-clock

---
+++ main.js
@@ -0,0 +1,15 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const min = document.querySelector('#min');
+const sec = document.querySelector('#sec');
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+    min.style.transform = `rotate(${(mm)}deg)`;
+    sec.style.transform = `rotate(${(ss)}deg)`;
+})
